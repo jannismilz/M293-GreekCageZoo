fcad5b15d7bf14b598d1f03763e44dd7364778ad
Jannis Milz <jannis@milz.ch>
2023-03-24 10:48:56 +0100

    Adjust Navbar & Footer to same height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import Logo from "./Logo";
 
 export default function Footer() {
     return (
-        <footer className="bg-beige-500 body-font">
-            <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+        <footer className="bg-beige-500 body-font py-4">
+            <div className="container mx-auto flex items-center sm:flex-row flex-col">
                 <Logo />
-                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-600 sm:py-2 sm:mt-0 mt-4">
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-600 sm:mt-0 mt-4">
                     © 2023 GreekCage Zoo
                 </p>
                 <div className="flex gap-8 sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import Logo from "./Logo";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center py-3 px-12 border-b shadow bg-beige-500">
-            <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+        <nav className="flex justify-between items-center py-4 bg-beige-500">
+            <div className="container mx-auto flex items-center sm:flex-row flex-col">
                 <Logo />
-                <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-8">
+                <div className="flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-8">
                     <a href="/">Home</a>
                     <a href="/information">Information</a>
                     <a href="/animals">Animals</a>
