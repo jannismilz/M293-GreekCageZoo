4b496a3c053efefe03da4efd538312c874b4f2f1
Jannis Milz <jannis@milz.ch>
2023-03-24 10:44:55 +0100

    Finish navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Logo from "./Logo";
+
 export default function Navbar() {
     return (
-        <>
-            <div className="text-2xl">Navbar</div>
-        </>
+        <nav className="flex justify-between items-center py-3 px-12 border-b shadow bg-beige-500">
+            <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+                <Logo />
+                <div className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-8">
+                    <a href="/">Home</a>
+                    <a href="/information">Information</a>
+                    <a href="/animals">Animals</a>
+                </div>
+            </div>
+        </nav>
     );
 }
