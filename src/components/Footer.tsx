cfc5c4f7142f6d134a01e1a56db5731415004e67
Jannis Milz <jannis@milz.ch>
2023-03-24 10:43:47 +0100

    Create footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
+import Logo from "./Logo";
+
 export default function Footer() {
     return (
-        <>
-            <div className="text-2xl">Footer</div>
-        </>
+        <footer className="bg-beige-500 body-font">
+            <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+                <Logo />
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-600 sm:py-2 sm:mt-0 mt-4">
+                    © 2023 GreekCage Zoo
+                </p>
+                <div className="flex gap-8 sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a href="/privacy">Privacy Policy</a>
+                    <a href="/imprint">Imprint</a>
+                </div>
+            </div>
+        </footer>
     );
 }
