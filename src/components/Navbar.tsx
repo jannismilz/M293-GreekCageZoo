ef7936b1599a41c2df7cba7880e7295b04bae98f
Jannis Milz <jannis@milz.ch>
2023-03-31 09:30:40 +0200

    Style Navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,24 @@ export default function Navbar() {
             <div className="container mx-auto flex items-center sm:flex-row flex-col">
                 <Logo />
                 <div className="flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start gap-8">
-                    <a href="/">Home</a>
-                    <a href="/information">Information</a>
-                    <a href="/animals">Animals</a>
+                    <a
+                        className="text-black/70 hover:text-black duration-100"
+                        href="/"
+                    >
+                        Home
+                    </a>
+                    <a
+                        className="text-black/70 hover:text-black duration-100"
+                        href="/information"
+                    >
+                        Information
+                    </a>
+                    <a
+                        className="text-black/70 hover:text-black duration-100"
+                        href="/animals"
+                    >
+                        Animals
+                    </a>
                 </div>
             </div>
         </nav>
