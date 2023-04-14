3990b628cb408eb811424253bee56e77869c2ff1
Jannis Milz <jannis@milz.ch>
2023-04-14 10:55:29 +0200

    Create `Animals` page

---
+++ src/pages/animals.tsx
@@ -0,0 +1,61 @@
+import AnimalCard from "@/components/AnimalCard";
+import fsPromises from "fs/promises";
+import path from "path";
+
+export type Animal = {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    age: number;
+    breed: string;
+};
+
+export default function Animals({ animals }: { animals: Animal[] }) {
+    return (
+        <section className="p-8">
+            <div className="m-auto container">
+                <h1 className="text-5xl font-semibold mb-10">Animals</h1>
+                <div className="flex flex-wrap w-full justify-between gap-y-8">
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                    {animals.map((animal: Animal) => (
+                        <AnimalCard animal={animal} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), "animals.json");
+    const jsonData = await fsPromises.readFile(filePath, "utf8");
+    const animals = JSON.parse(jsonData);
+
+    return {
+        props: animals,
+    };
+}
