59fc9f89dfc2c49e5960ea7d3fe4bc1ef761c1e6
Jannis Milz <jannis@milz.ch>
2023-04-21 10:32:28 +0200

    Add imprint

---
+++ src/pages/imprint.tsx
@@ -0,0 +1,13 @@
+export default function Imprint() {
+    return (
+        <div className="h-screenWithNavbar p-12 max-w-4xl m-auto">
+            <h1 className="text-4xl font-semibold mb-5">Imprint</h1>
+            <p>Jannis Milz</p>
+            <p>Sonnenbühlstrasse 37</p>
+            <p>8305 Dietlikon</p>
+            <p>Schweiz</p>
+            <p>079 789 64 74</p>
+            <p>zoo@jannismilz.com</p>
+        </div>
+    );
+}
