24deca7879957e27bfdf0fda8af6078e9c170068
Jannis Milz <jannis@milz.ch>
2023-05-12 10:56:08 +0200

    Test gh actions

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix;
-let basePath;
+// let assetPrefix;
+// let basePath;
 
-if (isGithubActions) {
-	// trim off `<owner>/`
-	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+// 	// trim off `<owner>/`
+// }
 
-	assetPrefix = `/${repo}/`;
-	basePath = `/${repo}`;
-}
+const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+assetPrefix = `/${repo}/`;
+basePath = `/${repo}`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	...(basePath &&
-		assetPrefix && { basePath: basePath, assetPrefix: assetPrefix }),
+	// ...(basePath &&
+	// 	assetPrefix && { basePath: basePath, assetPrefix: assetPrefix }),
+	basePath: basePath,
+	assetPrefix: assetPrefix,
 };
 
 module.exports = nextConfig;
