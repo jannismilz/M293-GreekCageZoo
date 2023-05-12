56e7a4de4e5a26aebfeac67501b7e2a7a656c13c
Jannis Milz <jannis@milz.ch>
2023-05-12 14:15:09 +0200

    Fix basePath env in url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,12 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<div
-				className={`h-screenWithNavbar bg-[url('${process.env.BASE_PATH}/background.jpeg')] bg-cover flex flex-col justify-center items-center`}
+				className={
+					`h-screenWithNavbar bg-[url(` +
+					process.env.BASE_PATH +
+					"/background.jpeg" +
+					`)] bg-cover flex flex-col justify-center items-center`
+				}
 			>
 				<h1 className="text-6xl text-white text-center">
 					Welcome to the paradise of greek mythology!
