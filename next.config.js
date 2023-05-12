7fd1956be31aa4b8608484f7f5c9b1e269c7b736
Jannis Milz <jannis@milz.ch>
2023-05-12 12:51:21 +0200

    Remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@
 // 	// trim off `<owner>/`
 // }
 
-const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-assetPrefix = `/${repo}/`;
-basePath = `/${repo}`;
+// assetPrefix = `/${repo}/`;
+// basePath = `/${repo}`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
