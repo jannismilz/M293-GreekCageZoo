9fcd9057d944346016bf9f34417995edd3e01b39
Jannis Milz <jannis@milz.ch>
2023-05-20 11:37:11 +0200

    Move website-wide metadate to `_app` for best practice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link
+					rel="icon"
+					href={
+						process.env.BASE_PATH
+							? process.env.BASE_PATH + "/favicon.svg"
+							: "/favicon.svg"
+					}
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,20 +6,7 @@ import Link from "next/link";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head>
-				<link
-					rel="icon"
-					href={
-						process.env.BASE_PATH
-							? process.env.BASE_PATH + "/favicon.svg"
-							: "/favicon.svg"
-					}
-				/>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Navbar />
 				<Main />
